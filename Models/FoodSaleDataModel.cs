a9d1fca05544e401ec1641f2aade849155a8e841
chawklyartun101423 <chawklyartun23@gmail.com>
2024-11-26 16:46:22 +0630

    cart view updated

---
+++ Models/FoodSaleDataModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodDelivery.Models
+{
+    public class FoodSaleDataModel
+    {
+        public int SaleId { get; set; }
+        public string? FoodId { get; set; }
+        public string? FoodName { get; set; }
+        public decimal FoodPrice { get; set; }
+        public string? FoodPhoto { get; set; }
+        public int Qty { get; set; }
+
+    }
+}
